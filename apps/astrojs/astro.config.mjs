0370a51f29ca86f9f8d04a80cc277eb320f23785
Graßl, Timon <Timon.Grassl@interhyp.de>
2022-12-12 14:23:47 +0100

    add astro hacker news

--- apps/astrojs/astro.config.mjs
+++ apps/astrojs/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [solid()],
+  adapter: node({
+    mode: 'standalone'
+  }),
+});
